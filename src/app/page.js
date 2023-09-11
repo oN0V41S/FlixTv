97ec1f9d8ca5766474923b90d8acf2d4c0c809b7
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-09-11 11:56:14 -0300

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="bg-slate-900 min-h-screen">
+      <div id="txt_welcome" className="text-l text-slate-100 pt-16 m-auto w-max uppercase font-bold">Bem Vindo Rafael</div>
+    </main>
+  )
+}
