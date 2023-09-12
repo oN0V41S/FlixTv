7275fee83e2f23d026acd9b426f070d5bb423170
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-09-12 20:26:19 +0000

    up

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function Home() {
-  return (
-    <main className="bg-slate-900 min-h-screen m-0 p-0">
-      <div id="txt_welcome" className="text-3xl text-slate-100 pt-32 m-auto w-max uppercase font-bold animate-pulse">bem vindo, rafael</div>
-    </main>
-  )
-}
