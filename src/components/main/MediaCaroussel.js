34276c88b2b57b04973743d9708b50a0707ec48c
ON0V41S <rafaelaugustonnovais@gmail.com>
2023-12-22 13:16:59 -0300

    update carousel

--- src/components/main/MediaCaroussel.js
+++ src/components/main/MediaCaroussel.js
@@ -1,33 +1,23 @@
 import Image from "next/image";
 
-<<<<<<< HEAD
 // Carrosel
 import Slider from "react-slick";
 import "@/styles/MediaCarrousel/mdcarousel.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-=======
-// Carrosel 
-import Slider from "react-slick"
-import '@/styles/MediaCarrousel/mdcarousel.css'
-import 'slick-carousel/slick/slick.css';
-import 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css'
->>>>>>> 3a8a23f6fc75eed369043664f1398e541bf928af
 
 import Naruto from "@/../../public/media/naruto.png";
 import Sasuke from "@/../public/media/sasuke.jpg";
 
 export default function MdCaroussel({ categoria }) {
   const settings = {
-    className: "center",
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4.01,
+    slidesToShow: 5,
     slidesToScroll: 3,
   };
 
-<<<<<<< HEAD
   return (
     <section>
       <h2 className="text-xl font-bold">{categoria}</h2>
@@ -41,15 +31,6 @@ export default function MdCaroussel({ categoria }) {
     </section>
   );
 }
-=======
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 5.1,
-        slidesToScroll: 2,
-    }
->>>>>>> 3a8a23f6fc75eed369043664f1398e541bf928af
 
 function Media({ name, src }) {
   return (
