280a0ba6dc0e13d88a6dd59f18d3f11beca83207
on0v41s <129956152+oN0V41S@users.noreply.github.com>
2023-12-27 03:13:23 +0000

    fix

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -1,9 +1,9 @@
-import { cadastro } from "../../../services/user";
+import { login } from "../../../services/user";
 
 export default function handler(req, res) {
   try {
-    const newUser = login(req.body);
-    res.status(201).json(newUser);
+    const user = login(req.body);
+    res.status(201).json(user);
   } catch (err) {
     res.status(400).json(err.message);
   }
