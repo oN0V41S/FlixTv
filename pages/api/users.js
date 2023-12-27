28f0602513d94e8624738aededc96e62c611ca9a
on0v41s <129956152+oN0V41S@users.noreply.github.com>
2023-12-27 01:03:36 +0000

    refactor: Update form layout and component in login and register pages
    Updated form layout and component in login and register pages

--- pages/api/users.js
+++
@@ -1,2 +0,0 @@
-import fetch from "node-fetch";
-import db from "@/../data/data.db";
--- pages/login.js
+++ pages/login.js
@@ -5,22 +5,16 @@ import SubmitForm from "@/components/form/submit";
 
 export default function Login() {
   return (
-    <form className="mt-3 m-auto w-12/12 lg:w-max p-16 rounded-xl text-white bg-slate-900">
-      <div className="flex flex-col gap-6">
-        <div className="font-bold text-xl text-left	">Faça Login</div>
-        <InputForm type="text" placeholder="insira seu Usuário" />
-        <InputForm type="password" placeholder="insira sua Senha" />
+    <form className="mt-3 m-auto w-12/12 lg:w-max p-16 rounded-xl text-white bg-slate-900 flex flex-col gap-4">
+      <div className="font-bold text-xl text-left	">Faça Login</div>
+      <InputForm type="text" placeholder="insira seu Usuário" />
+      <InputForm type="password" placeholder="insira sua Senha" />
+      <SubmitForm href="/users" width="full" />
+      <div className="w-[50%] flex gap-2">
+        <input type="checkbox" />
+        <div>Me lembre</div>
       </div>
-      <div className="mt-10">
-        <SubmitForm href="/users" width="full" />
-        <div className="flex mt-4">
-          <div className="w-2/4 flex gap-2">
-            <input type="checkbox" />
-            <div>Me lembre</div>
-          </div>
-          <div className="w-2/4 text-right">Precisa de Ajuda?</div>
-        </div>
-      </div>
+      <div className="w-[50%] text-right">Precisa de Ajuda?</div>
     </form>
   );
 }
