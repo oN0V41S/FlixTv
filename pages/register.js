2fbeb9f23e4bb9f4136d78764a720dbbf11f667c
on0v41s <129956152+oN0V41S@users.noreply.github.com>
2023-12-27 04:56:30 +0000

    fix form error

--- pages/register.js
+++ pages/register.js
@@ -12,6 +12,8 @@ export default function Register() {
     password: '',
   })
 
+
+
   const handleFormEdit = (event,name) => {
     setFormData({
       // Insere todos dados de formData em setFormData
@@ -21,31 +23,20 @@ export default function Register() {
     })
   } 
 
-  const handleForm = async (event) => {
-    try{
-      event.preventDefault()
-      const response = await fetch('/api/user/cadastro', {
-        method: 'POST',
-        body: JSON.stringify(formData)
-      })
-
-      const [erro, setErro] = useState
-
-      const json = await response.json()
-      if(response.status !== 201) throw new Error(json)
-
-    } catch (err){
-
-    }
+  // Validar Form foi enviado
+  const handleForm = (event) => {
+    event.peventDefault()
+    console.log(formData)
   }
 
+
   return (
-    <form className="mt-3 m-auto w-12/12 lg:w-max p-16 rounded-xl text-white bg-slate-900 flex flex-col gap-4">
+    <form onSubmit={handleForm} className="mt-3 m-auto w-12/12 lg:w-max p-16 rounded-xl text-white bg-slate-900 flex flex-col gap-4">
       <h1 className="font-bold text-xl text-left	">Cadastre sua conta</h1>
-      <InputForm type="text" value={formData.name} placeholder="Seu primeiro e ultimo Nome" onChange={(event) => {handleFormEdit(event, 'name')}} required/>
-      <InputForm type="email" value={formData.email} placeholder="Seu Email" onChange={(event) => {handleFormEdit(event, 'email')}} required/>
-      <InputForm type="password" value={formData.password} placeholder="Sua Senha" onChange={(event) => {handleFormEdit(event, 'password')}} required/>
-      <SubmitForm width="full" placeholder="Registrar" onClick={handleForm} />
+      <InputForm type="text" placeholder="Seu primeiro e ultimo Nome" required value={formData.name}/>
+      <InputForm type="email" placeholder="Seu Email" required value={formData.email}/>
+      <InputForm type="password" placeholder="Sua Senha" required value={formData.password}/>
+      <SubmitForm width="full" placeholder="Registrar" />
       <Link href="/login" className="w-[50%]">
         já Possui conta?
       </Link>
