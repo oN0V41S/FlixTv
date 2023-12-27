2f65cf746da51b28a7507af165ae076c54c72a24
on0v41s <129956152+oN0V41S@users.noreply.github.com>
2023-12-27 01:17:34 +0000

    style: Change h1 text in register page
    Changed the text on the h1 element in the register page.

--- pages/register.js
+++ pages/register.js
@@ -5,7 +5,7 @@ import SubmitForm from "@/components/form/submit";
 export default function Register() {
   return (
     <form className="mt-3 m-auto w-12/12 lg:w-max p-16 rounded-xl text-white bg-slate-900 flex flex-col gap-4">
-      <h1 className="font-bold text-xl text-left	">Register</h1>
+      <h1 className="font-bold text-xl text-left	">Cadastre sua conta</h1>
       <InputForm type="text" placeholder="Seu primeiro e ultimo Nome" />
       <InputForm type="email" placeholder="Seu Email" />
       <InputForm type="password" placeholder="Sua Senha" />
